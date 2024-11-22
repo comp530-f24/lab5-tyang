3793d78d63e30b026dc39fea9585a77870838d17
Tyler Yang <88322118+tylerliuyang@users.noreply.github.com>
2024-11-22 12:44:38 -0500

    F

--- python/measure.py
+++ python/measure.py
@@ -56,7 +56,8 @@ def perform_io_test(file_path, io_size, stride=0, is_random=False, is_write=True
         else:
             # make read_size a multiple of 512
             m = mmap.mmap(-1, io_size)
-            f = os.fdopen(fd, 'rb', closefd=False)
+            # f = os.fdopen(fd, 'rb', closefd=False)
+            f = open(fd, 'rb', closefd=False)
             f.readinto(m)
         
         # Force sync for write operations
